772ccfeb6b725880014ee45b275e4ef19ca6862c
Kevin <kvnnedd@gmail.com>
2025-11-28 01:58:49 -0500

    Refactor: update main bem naming and add new section for card

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,13 @@ import "./Main.css";
 import SearchForm from "../SearchForm/SearchForm.jsx";
 import About from "../About/About.jsx";
 import hero from "../../assets/hero.png";
+import NewsCard from "../NewsCard/NewsCard.jsx";
 
 export const Main = ({ searchQuery, setSearchQuery, onSubmit }) => {
   return (
-    <main className="main__container">
-      <img className={"hero"} src={hero} alt="" />
-      <div className="main__main">
+    <main className="main">
+      <img className={"main__hero-bg"} src={hero} alt="" />
+      <section className="main__hero">
         <h1 className="main__title">
           What's going on in
           <br /> the world?
@@ -16,12 +17,17 @@ export const Main = ({ searchQuery, setSearchQuery, onSubmit }) => {
           Find the lastest news on any topic and save the in your personal
           account.
         </h2>
-      </div>
+      </section>
       <SearchForm
         searchQuery={searchQuery}
         setSearchQuery={setSearchQuery}
         onSubmit={onSubmit}
       />
+      <section className={"main__content"}>
+        <h1 className={"main__content-title"}>Search results</h1>
+
+        <NewsCard />
+      </section>
       <About />
     </main>
   );
---
+++ src/components/NewsCard/NewsCard.jsx
@@ -0,0 +1,6 @@
+import "./NewsCard.css";
+
+const NewsCard = () => {
+  return <div className="newsCard-container">NewsCard</div>;
+};
+export default NewsCard;
