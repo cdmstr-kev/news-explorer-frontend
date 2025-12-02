e7a097bd94c0974d74a5fb684be1fcf35d9bca5f
Kevin <kvnnedd@gmail.com>
2025-12-02 10:34:57 -0500

    saved page first draft complete

--- src/components/SavedNews/SavedNews.jsx
+++ src/components/SavedNews/SavedNews.jsx
@@ -1,15 +1,24 @@
 import NewsCard from "../NewsCard/NewsCard.jsx";
+import "./SavedNews.css";
 
 const SavedNews = () => {
   return (
     <div className={"saved-news__container"}>
       <p className={"saved-news__articles"}>Saved articles</p>
-      <h1 className={"saved-news__title"}>Elise, You have 5 saved articles</h1>
+      <h1 className={"saved-news__title"}>
+        Elise, You have 5 saved
+        <br /> articles
+      </h1>
       <p className={"saved-news__subtitle"}>
-        By keywords: Nature, Yellowstone, and 2 other
+        By keywords:{" "}
+        <span className={"saved-news__subtitle_type_bold"}>
+          Nature, Yellowstone, and 2 other
+        </span>
       </p>
       <img src="" alt="" />
-      <section className={"saved-news__cardlist"}></section>
+      <section className={"saved-news__cardlist"}>
+        <p>The cards go in here</p>
+      </section>
     </div>
   );
 };
