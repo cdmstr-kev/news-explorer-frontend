318e18c4418b04d4d5fa42d7ad98281d03b8fa1f
Kevin <kvnnedd@gmail.com>
2025-12-10 06:25:57 -0500

    Update auth.js with simulated promises

---
+++ src/utils/auth.js
@@ -0,0 +1,36 @@
+import { validateUserCredentials, saveUserToStorage } from "./helpers.js";
+
+export const authorize = (email, password) => {
+  return new Promise((resolve, reject) => {
+    const user = validateUserCredentials(email, password);
+
+    if (!user) {
+      return reject(new Error("Invalid Credentials"));
+    }
+
+    resolve({
+      token: `fake-token-${Date.now()}`,
+      user: user,
+    });
+  });
+};
+
+export const checkToken = (token) => {
+  return new Promise((resolve, reject) => {
+    if (token) {
+      resolve({ valid: true });
+    } else {
+      reject(new Error("Invalid token"));
+    }
+  });
+};
+
+export const register = (userData) => {
+  return new Promise((resolve, reject) => {
+    saveUserToStorage(userData);
+    resolve({
+      message: "Registration successful",
+      user: userData,
+    });
+  });
+};
