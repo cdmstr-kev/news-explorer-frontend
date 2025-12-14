5329750d09beeb64a4b9b1e81e7a72fdf7e5d843
Kevin <kvnnedd@gmail.com>
2025-12-14 12:54:06 -0500

    Update about section to include author details

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import avatarPlaceholder from "../../assets/avatar-placeholder.png";
 import "./About.css";
+import authorAvatar from "../../assets/author-avatar.jpg";
 
 const About = () => {
   return (
@@ -7,18 +8,23 @@ const About = () => {
       <section className="about__author">
         <img
           className={"about__author-avatar"}
-          src={avatarPlaceholder}
+          src={authorAvatar}
           alt={"Portrait of web developer"}
         />
         <div className="about__author-info">
           <h2 className="about__author-title">About the author</h2>
           <p className="about__author-desc">
-            This block describes the project author. Here you should indicate
-            your name, what you do, and which development technologies you know.
+            I'm Kevin, a frontend developer passionate about building things
+            that matter. I work primarily with React and JavaScript, focusing on
+            creating web applications that are both beautiful and functional.
+            Good code should be invisible to users but obvious to other
+            developers.
           </p>
           <p className="about__author-desc">
-            You can also talk about your experience with TripleTen, what you
-            learned there, and how you can help potential customers.
+            My journey through TripleTen's Web Development Bootcamp transformed
+            how I think about building for the web. Now I'm looking for
+            opportunities to create impactful digital experiences. If you're
+            building something meaningful, I'd love to hear about it.
           </p>
         </div>
       </section>
