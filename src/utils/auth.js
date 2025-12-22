2f7a89a48ffc90d9f8b7683a36aa1d5813435687
Kevin <kvnnedd@gmail.com>
2025-12-22 11:31:11 -0500

    refactor: update auth.js to connec with the backend for authentication

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,36 +1,80 @@
 import { validateUserCredentials, saveUserToStorage } from "./helpers.js";
 
-export const authorize = (email, password) => {
-  return new Promise((resolve, reject) => {
-    const user = validateUserCredentials(email, password);
+const backendBaseUrl = import.meta.env.VITE_BACKEND_API_URL;
 
-    if (!user) {
-      return reject(new Error("Invalid Credentials"));
-    }
+const checkResponse = (res) => {
+  if (res.ok) {
+    return res.json();
+  }
 
-    resolve({
-      token: `fake-token-${Date.now()}`,
-      user: user,
+  return res
+    .json()
+    .then((data) => {
+      const error = new Error(
+        data.message || `HTTP ${res.status}: ${res.statusText}`
+      );
+      error.status = res.status;
+      return Promise.reject(error);
+    })
+    .catch((parseError) => {
+      const error = new Error(`HTTP ${res.status}: ${res.statusText}`);
+      error.status = res.status;
+      return Promise.reject(error);
     });
-  });
 };
 
-export const checkToken = (token) => {
-  return new Promise((resolve, reject) => {
-    if (token) {
-      resolve({ valid: true });
-    } else {
-      reject(new Error("Invalid token"));
-    }
-  });
+export const login = (email, password) => {
+  return fetch(`${backendBaseUrl}/users/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then(checkResponse)
+    .then((data) => {
+      if (data.token) {
+        localStorage.setItem("jwt", data.token);
+      }
+      return data;
+    });
+};
+
+export const getCurrentUser = () => {
+  const token = localStorage.getItem("jwt");
+
+  if (!token) {
+    return Promise.reject(new Error("No token found"));
+  }
+
+  return fetch(`${backendBaseUrl}/users/me`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer  + ${token}`,
+    },
+  }).then(checkResponse);
+};
+
+export const checkToken = () => {
+  return getCurrentUser;
 };
 
 export const register = (userData) => {
-  return new Promise((resolve) => {
-    saveUserToStorage(userData);
-    resolve({
-      message: "Registration successful",
-      user: userData,
+  const { email, password } = userData;
+
+  return fetch(`${backendBaseUrl}/users/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password, username }),
+  })
+    .then(checkResponse)
+    .then((data) => {
+      if (data.token) {
+        localStorage.setItem("jwt", data.token);
+      }
+      return data;
     });
-  });
 };
